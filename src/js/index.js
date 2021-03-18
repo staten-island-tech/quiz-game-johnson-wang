f4e6604b94f221889f25057002ebc93501591308
J0hns0n-Wang <johnson.wang23@sitechhs.com>
2021-03-18 13:07:56 -0400

    :)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,48 @@
 console.log("connected");
+
+const questions = [
+  {
+    number: 1,
+    question: "What is 1+1?",
+    choices: {
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4",
+    },
+    answer: "2",
+  },
+  {
+    number: 2,
+    question: "What is 2+1?",
+    choices: {
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4",
+    },
+    answer: "3",
+  },
+  {
+    number: 3,
+    question: "What is 2+2?",
+    choices: {
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4",
+    },
+    answer: "4",
+  },
+  {
+    number: 4,
+    question: "What is 0+9?",
+    choices: {
+      a: "9",
+      b: "10",
+      c: "11",
+      d: "12",
+    },
+    answer: "9",
+  },
+];
