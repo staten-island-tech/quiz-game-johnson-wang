d7a7394ff6852a13fed6526f0ab0c356d6c92a5b
J0hns0n-Wang <johnson.wang23@sitechhs.com>
2021-03-19 09:13:10 -0400

    ythjytudj

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,3 @@
 console.log("connected");
-
-const questions = [
-  {
-    number: "1",
-    question: "What is 1+1?",
-    choices: ["0", "1", "2", "3"],
-    answer: 2,
-  },
-  {
-    number: "2",
-    question: "What is 2+1?",
-    choices: ["0", "1", "2", "3"],
-    answer: 3,
-  },
-  {
-    number: "3",
-    question: "What is 2+2?",
-    choices: ["1", "2", "3", "4"],
-    answer: 3,
-  },
-  {
-    number: "4",
-    question: "What is 0+9?",
-    choices: ["9", "10", "11", "12"],
-    answer: 0,
-  },
-];
+import { DOMSelectors } from "./DOMSelectors";
+import { questions } from "./questions";
---
+++ src/js/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    number: "1",
+    question: "What is 1+1?",
+    choices: ["0", "1", "2", "3"],
+    answer: "2",
+  },
+  {
+    number: "2",
+    question: "What is 2+1?",
+    choices: ["0", "1", "2", "3"],
+    answer: "3",
+  },
+  {
+    number: "3",
+    question: "What is 2+2?",
+    choices: ["1", "2", "3", "4"],
+    answer: "4",
+  },
+  {
+    number: "4",
+    question: "What is 0+9?",
+    choices: ["9", "10", "11", "12"],
+    answer: "9",
+  },
+];
+
+export { questions };
