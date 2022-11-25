554cc2ea113bbb4ffd89530f0999400df274617d
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-11-24 21:27:19 -0500

    done i think

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,24 +1,39 @@
 const questions = [
   {
-    question: "1. What is 1+1?",
-    choices: ["0", "1", "2", "3"],
-    answer: "2",
+    question: "1. Before the creation of the world, there was an endless expanse of water. Who is the god that personifies these ancient waters?",
+    choices: ["Nun", "Shu", "Re", "Atum"],
+    answer: "Nun",
   },
   {
-    question: "2. What is 2+1?",
-    choices: ["0", "1", "2", "3"],
-    answer: "3",
+    question: "2. How were humans created?",
+    choices: ["The god Atum repaired the fallen pillars holding up the sky.",
+     "Humans were created from tree trunks",
+      "Humans were created from Nun",
+       "Humans were created after the eye of Re separated from him "],
+    answer: "Humans were created after the eye of a Re separated from him ",
   },
   {
-    question: "3. What is 2+2?",
-    choices: ["1", "2", "3", "4"],
-    answer: "4",
+    question: "3. Re gave birth to the twins: Shu and Tefnut. Which of the following represents or is best associated with these two gods?",
+    choices: ["The sky", "The atmosphere", "The desert", "The sun and moon"],
+    answer: "The atmosphere",
   },
   {
-    question: "4. What is 0+9?",
-    choices: ["9", "10", "11", "12"],
-    answer: "9",
+    question: "4. Why did Re decide to ascend into heaven?",
+    choices: ["He was old and could not think properly",
+     "He died during the first rebellion", "He was in pain of the problems caused by the first rebellion",
+      "Other gods were awaiting his arrival in heaven"],
+    answer: "He was in pain of the problems caused by the first rebellion",
   },
+  {
+    question: "5. What obstructions were in Re’s journey from the east every morning?",
+    choices: [
+      "The boat was too heavy so it took longer for the sun to go around",
+      "A giant serpent confronted Re and his crew every morning",
+      "A giant crocodile was in their way",
+      "A giant bird tackled the boat every morning"
+    ],
+    answer: "A giant serpent confronted Re and his crew every morning"
+  }
 ];
 
 export { questions };
